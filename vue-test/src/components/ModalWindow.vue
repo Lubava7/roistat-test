ef5c73065bb89bea25beaf7d18109140544b6788
lubava7 <joheunsaram@mail.ru>
2022-07-11 01:48:42 +0300

    edit

---
+++ vue-test/src/components/ModalWindow.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <div class="modal-header" name="header">
+        Добавление пользователя
+      </div>
+      <div class="modal-body" name="body">
+        <div class="body-part">
+          <div>Имя</div>
+          <div>Телефон</div>
+          <div>Начальник</div>
+        </div>
+
+        <div class="body-part">
+          <input
+            type="text"
+            @click="hideText"
+            v-bind:value="names"
+            @input="names = $event.target.value"
+          />
+          <input
+            type="number"
+            @click="hideText"
+            v-bind:value="numbers"
+            @input="numbers = $event.target.value"
+          />
+          <select>
+            <option>Родитель динамично</option>
+          </select>
+        </div>
+      </div>
+      <div class="modal-footer" name="footer">
+        <button @click="close">Сохранить</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {
+      names: "",
+      numbers: ""
+    };
+  },
+  mounted() {
+    if (localStorage.names) {
+      this.names = localStorage.names;
+    }
+    if (localStorage.numbers) {
+      this.numbers = localStorage.numbers;
+    }
+  },
+  methods: {
+    close() {
+      localStorage.names = this.names;
+      localStorage.numbers = this.numbers;
+      this.$emit("close");
+    },
+    hideText() {
+      this.names = "";
+      this.nambers = "";
+    }
+  }
+};
+</script>
+<style>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.modal {
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: flex-start;
+  padding: 0 0 0 30px;
+  width: 350px;
+  height: 300px;
+}
+
+.modal-header,
+.modal-footer {
+  display: flex;
+  justify-content: center;
+}
+.modal-header {
+  justify-content: center;
+}
+
+.modal-footer {
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.modal-footer button {
+  height: 30px;
+  width: 100px;
+  border-radius: 17px;
+}
+
+.modal-body {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 50%;
+}
+.body-part {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin: 0 10px 0 0;
+}
+</style>
---
+++ vue-test/src/components/TableForm.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="hello">
+    <div class="table_button">
+      <button class="add_button" @click="showModal" type="button">
+        Добавить
+      </button>
+      <div class="table">
+        <div class="name_column">
+          <div class="name">Имя</div>
+          <div class="dinamic-name">{{ dinamic_name }}</div>
+          <div class="dinamic-name">{{ dinamic_name }}</div>
+          <div class="dinamic-name">{{ dinamic_name }}</div>
+          <div class="dinamic-name">{{ dinamic_name }}</div>
+        </div>
+
+        <div class="number_column">
+          <div class="phone_number">Телефон</div>
+          <div class="dinamic-number">{{ dinamic_number }}</div>
+          <div class="dinamic-number">{{ dinamic_number }}</div>
+          <div class="dinamic-number">{{ dinamic_number }}</div>
+          <div class="dinamic-number">{{ dinamic_number }}</div>
+        </div>
+      </div>
+    </div>
+    <ModalWindow v-show="isModalVisible" @close="closeModal" />
+  </div>
+</template>
+
+<script>
+import ModalWindow from "./ModalWindow.vue";
+export default {
+  name: "TableForm",
+  components: {
+    ModalWindow
+  },
+  data() {
+    return {
+      isModalVisible: false,
+      dinamic_name: "Liubava",
+      dinamic_number: "+7 977 325 53 35"
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.hello {
+  display: flex;
+  flex-direction: row;
+}
+.table_button {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+}
+.table {
+  width: 400px;
+  display: flex;
+  flex-direction: row;
+}
+.name_column {
+  border: 2px solid black;
+  width: 160px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.name {
+  border-bottom: 0.5px solid black;
+  width: 148px;
+  height: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 3px 3px 3px 10px;
+}
+.number_column {
+  border: 2px solid black;
+  width: 240px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.phone_number {
+  border-bottom: 0.5px solid black;
+  width: 229px;
+  height: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 3px 3px 3px 10px;
+}
+.dinamic-name {
+  border-bottom: 0.5px solid black;
+  display: flex;
+  height: 100%;
+  width: 97%;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px 115px 10px 10px;
+}
+.dinamic-number {
+  border-bottom: 0.5px solid black;
+  height: 100%;
+  width: 97%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 10px 0 10px 10px;
+}
+.add_button {
+  border-radius: 17px;
+  height: 30px;
+  width: 100px;
+  font-size: 17px;
+  margin: 10px;
+}
+</style>
